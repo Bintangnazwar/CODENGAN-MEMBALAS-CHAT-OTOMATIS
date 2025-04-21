d4a4c06f262ec2bc158f3437729b1f9d36d0222c
BINTANG NAZWAR <160728003+Bintangnazwar@users.noreply.github.com>
2025-04-21 11:02:41 +0700

    Add files via upload

---
+++ PesanWA.py
@@ -0,0 +1,10 @@
+import pywhatkit
+import pyautogui
+import time
+
+pywhatkit.sendwhatmsg_instantly("+62 nomor wa kalian","Halo saya  ",16,09)
+pywhatkit.sendwhatmsg_instantly("+62 nomo wa kalian","Halo saya",16,10)
+time.sleep(10)
+pyautogui.click()
+pyautogui.press('enter')
+
